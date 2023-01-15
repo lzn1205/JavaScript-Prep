fbc86735339bb416332c028929c9c054d607c7a4
Long Nguyen <116136795+lzn1205@users.noreply.github.com>
2023-01-15 11:27:52 -0600

    Create 83-Remove-Duplicates-from-Sorted-List.js

---
+++ Two-Pointer/83-Remove-Duplicates-from-Sorted-List.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    var cur = head;
+    while (cur && cur.next) {
+        if (cur.next.val === cur.val) {
+            cur.next = cur.next.next;
+        }
+        else cur = cur.next;
+    }
+    return head;
+};
