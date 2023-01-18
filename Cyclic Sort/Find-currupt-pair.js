a14aba010ac4c10fd03592e34e513f871f7088d3
Long Nguyen <116136795+lzn1205@users.noreply.github.com>
2023-01-18 11:38:26 -0600

    Update Find-currupt-pair.js

--- Cyclic Sort/Find-currupt-pair.js
+++ Cyclic Sort/Find-currupt-pair.js
@@ -1,22 +1,23 @@
-var findCurruptPair = function(arr) {
-  let 
-  if (arr.length === 0) return arr;
+function printTwoElements(arr) {
+  var i;
+    document.write("The repeating element is: ");
+  for (i = 0; i < arr.length; i++) {
+    let num = Math.abs(arr[i]);
+    if (arr[num - 1] > 0) arr[num - 1] = arr[num - 1] * -1;
+    else document.write(num);
+  }
   
-  let i = 0;
-  while (i < arr.length) {
-    if (arr[i] != arr[arr[i] - 1]) {
-      let idx = arr[i] - 1;
-      
-      let x = arr[i];
-      arr[i] = arr[idx];
-      arr[idx] = x;
-    } else {
-      i++;
-    }
+  document.write("<br> and the missing element is ");
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) document.write(i + 1);
   }
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] != i + 1) {
-      return new 
-    }
-  }
-};
+}
+// Running
+arr = new Array(7, 3, 4, 5, 5, 6, 2);
+printTwoElements(arr);
+// Output: 
+// The repeating element: 5
+// and the missing element: 1
+
+// Time Complexity: O(n)
+// Space Complexity: O(1) as it is using constant variables.
